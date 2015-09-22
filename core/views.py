038c4bdca7e3a9d1c4ef14242303e8f612a38fa8
amandamaturo <amanda.maturo@quinnipiac.edu>
2015-09-22 18:22:15 +0000

    Add question create view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
+
+class QuestionCreateView(CreateView):
+    model = Question
+    template_name = "question/question_form.html"
+    fields = ['title', 'description']
+    success_url = reverse_lazy('home')
+
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
