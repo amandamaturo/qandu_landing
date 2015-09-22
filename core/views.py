9977619a321570bf478f4a8a9a053f1155e5c364
amandamaturo <amanda.maturo@quinnipiac.edu>
2015-09-22 18:31:26 +0000

    Add success page

--- core/views.py
+++ core/views.py
@@ -8,9 +8,12 @@ class QuestionCreateView(CreateView):
     model = Question
     template_name = "question/question_form.html"
     fields = ['title', 'description']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('success')
 
 from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Success(TemplateView):
+  template_name = "success.html"
